ddc33dd0be1f1ddf4f1a07fa8c8d2e6260c43695
Muhammad Amman <developercity316@gmail.com>
2024-05-21 15:56:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 
 corsAnywhere.createServer({
   originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: [], // Do not require any specific headers
   removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
   console.log(`Running CORS Anywhere on ${host}:${port}`);
